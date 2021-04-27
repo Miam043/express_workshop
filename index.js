1241fc0e1b1e5d5bd777a27f3615eaaf60fcabf0
Milton Alvarez <restartm04@gmail.com>
2021-04-27 17:17:13 -0500

    Actividad 13

--- index.js
+++ index.js
@@ -3,23 +3,21 @@ const express = require('express');
 const app = express();
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user');
+const auth =require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 
-app.get("/",(req, res, next) =>{
-  return res.status(200).json({code: 1,message: "Bienvenido al pokedex"});
- });
+app.get("/",);
 
+ app.use("/user", user);
+ app.use(auth);
  app.use("/pokemon",pokemon);
- app.use("/user", user)
-
- app.use((req,res,next) => {
-   return res.status(404).json({code: 404, message:"URL no encontrada"});
-
- })
+ app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () =>{
 console.log("Server is running...")
